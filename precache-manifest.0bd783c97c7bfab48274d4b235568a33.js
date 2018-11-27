733fcb8fba640a2e8783544541d7302332fe8cdf
John Walker <john_walker_333@hotmail.com>
2018-11-27 21:50:51 +0000

    Updates

---
+++ precache-manifest.0bd783c97c7bfab48274d4b235568a33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4d3893bf009e7d807f7f04eef5a0e1f",
+    "url": "/portfolio/static/media/stealthModePreview.e4d3893b.png"
+  },
+  {
+    "revision": "4f7b8ee4f38eb6ee566d9ef6f50d13fb",
+    "url": "/portfolio/static/media/shooterPreview.4f7b8ee4.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a8527e4d5e4d9e0d722c",
+    "url": "/portfolio/static/js/main.a8527e4d.chunk.js"
+  },
+  {
+    "revision": "a10772c9c8e6cfa27406",
+    "url": "/portfolio/static/js/1.a10772c9.chunk.js"
+  },
+  {
+    "revision": "a8527e4d5e4d9e0d722c",
+    "url": "/portfolio/static/css/main.7823abe1.chunk.css"
+  },
+  {
+    "revision": "a10772c9c8e6cfa27406",
+    "url": "/portfolio/static/css/1.7785f1ba.chunk.css"
+  },
+  {
+    "revision": "f1de2be7e7ce72539669735ea33cfc34",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.005cee9344b2e6282b9e269e76a95d24.js"
+  "/portfolio/precache-manifest.0bd783c97c7bfab48274d4b235568a33.js"
 );
 
 workbox.clientsClaim();
